b5142148be775d74b40e37229c98ab4d3a58075b
Elvitilianti19220825 <k94714877@gmail.com>
2023-09-25 16:26:04 +0700

    Controller Penerbit

---
+++ app/Controllers/Penerbit.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Penerbit extends BaseController
+{
+    public function Nama_penerbit()
+    {
+        //
+    }
+    public function Tahun_terbit()
+    {
+        //
+    }
+}
