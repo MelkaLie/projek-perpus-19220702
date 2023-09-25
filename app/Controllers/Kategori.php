14181881e0481c4de8e8adbb28045f247e1dd8c4
MelkaLie <melkaandriana627@gmail.com>
2023-09-25 15:48:46 +0700

    controller kategori

---
+++ app/Controllers/Kategori.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Kategori extends BaseController
+{
+    public function dewasa()
+    {
+        //
+    }
+    public function anak_anak()
+
+    {
+        //
+    }
+}
